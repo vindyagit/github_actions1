a8c6fb57a11582106d27bef1ccf6692284bb5049
vindyagit <vindyachandru93@gmail.com>
2025-08-30 15:13:09 +0530

    Update file_test.py

--- file_test.py
+++ file_test.py
@@ -3,12 +3,12 @@
 def test_calc_addition():
     output = 2 + 4
     assert output == 6
-    
-    
+
+
 def test_calc_substraction():
     output = 2 - 4
     assert output == -2
-  
+
 
 def test_calc_multiply():
     output = 2 * 4
