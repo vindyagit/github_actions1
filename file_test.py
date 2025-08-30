9fe98c01d13134a7d72bf76ac81f2ecd88cb2171
vindyagit <vindyachandru93@gmail.com>
2025-08-30 14:23:19 +0530

    Update file_test.py

--- file_test.py
+++ file_test.py
@@ -1,19 +1,15 @@
-def test_calc_addition():
-  # Function test the output of 2+4
+deftest_calc_addition():
     output = 2 + 4
     assert output == 6
   
-def test_calc_substraction():
-  # Function test the output of 2-4
+deftest_calc_substraction():
     output = 2 - 4
     assert output == -2
   
-def test_calc_multiply():
-  # Function test the output of 2*4
+deftest_calc_multiply():
     output = 2 * 4
     assert output == 8
   
-def test_coucou():
-  # Function test if the output return 'hello'
+deftest_coucou():
     output = 'hello'
     assert output == 'hello'
